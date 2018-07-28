249938dead0d733723e7c23580e7a4c36a54c812
exs007 <007exs@gmail.com>
2018-07-28 16:10:58 -0400

    OrderService implementation

--- src/CheckoutKataAPI/Services/OrderService.cs
+++ src/CheckoutKataAPI/Services/OrderService.cs
@@ -6,6 +6,7 @@ using CheckoutKataAPI.DAL;
 using CheckoutKataAPI.Entities;
 using CheckoutKataAPI.Entities.Orders;
 using CheckoutKataAPI.Entities.Products;
+using CheckoutKataAPI.Exceptions;
 using CheckoutKataAPI.Models;
 using CheckoutKataAPI.Workflow.Orders;
 
@@ -28,22 +29,97 @@ namespace CheckoutKataAPI.Services
 
         public Order GetOrder(int id)
         {
-            throw new NotImplementedException();
+            var order = GetOrderFromRepository(id);
+            order = GetRecalculatedOrder(order);
+
+            return order;
         }
 
         public Order CreateNewOrder()
         {
-            throw new NotImplementedException();
+            var order = new Order();
+            order = _orderRepository.Add(order);
+
+            return order;
         }
 
         public Order AddProductInOrder(int idOrder, AddOrderToProductModel item)
         {
-            throw new NotImplementedException();
+            if (item == null)
+            {
+                throw new AppValidationException("Add product model isn't specififed");
+            }
+
+            var order = GetOrderFromRepository(idOrder);
+            var product = GetProduct(item.SKU);
+
+            var orderToProduct = order.OrderToProducts.FirstOrDefault(p=>p.IdProduct==product.Id);
+            if (orderToProduct == null)
+            {
+                orderToProduct = new OrderToProduct()
+                {
+                    IdProduct=product.Id,
+                };
+                order.OrderToProducts.Add(orderToProduct);
+            }
+            orderToProduct.QTY += item.QTY;
+            order = GetRecalculatedOrder(order);
+
+            var result = _orderRepository.Update(order);
+            return result ? order : null;
         }
 
         public Order DeleteProductInOrder(int idOrder, string sku)
         {
-            throw new NotImplementedException();
+            var order = GetOrderFromRepository(idOrder);
+            var product = GetProduct(sku);
+
+            var orderToProductForDelete = order.OrderToProducts.FirstOrDefault(p=>p.IdProduct==product.Id);
+            if (orderToProductForDelete == null)
+            {
+                throw new AppValidationException("The given product doesn't exist in the order");
+            }
+
+            if (orderToProductForDelete.IdUsedPromotion.HasValue)
+            {
+                throw new AppValidationException("Deleting promotion product isn't permitted");
+            }
+
+            order.OrderToProducts.Remove(orderToProductForDelete);
+            order = GetRecalculatedOrder(order);
+
+            var result = _orderRepository.Update(order);
+            return result ? order : null;
+        }
+
+        private Order GetOrderFromRepository(int id)
+        {
+            var order = _orderRepository.Select(id);
+            if (order == null)
+            {
+                throw new AppValidationException("Invalid order id");
+            }
+
+            return order;
+        }
+
+        private Product GetProduct(string sku)
+        {
+            var product = _productService.GetProduct(sku);
+            if (product==null)
+            {
+                throw new AppValidationException("Invalid product SKU");
+            }
+
+            return product;
+        }
+
+        private Order GetRecalculatedOrder(Order order)
+        {
+            var context = new OrderCalculationContext(order);
+            context = _calculationProcessor.CalculateOrder(context);
+
+            return context.GetResultOrder();
         }
     }
 }
